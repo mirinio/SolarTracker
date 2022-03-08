1f1d52bb6e65f19744b331f01f8e8aedc1750858
Amir Awak <amir.kian@awak.ch>
2022-03-08 18:37:43 +0100

    move it with arrow keys

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Device.Pwm;
 
+
 namespace Solartracker
 {
     internal class Program
@@ -11,18 +12,52 @@ namespace Solartracker
             Console.WriteLine("--- TEST 1 ---");
 
             var pwm = PwmChannel.Create(0, 1, 50);
-
             pwm.DutyCycle = 0.01;
+            Angle(pwm, 45);
 
-            Angle(pwm, 50);
+            bool start = true;
+            int currentAngle = 0;
 
+            while (start)
+            {
+                ConsoleKeyInfo keyinfo = Console.ReadKey(true);
+
+                switch (keyinfo.Key)
+                {
+                    case ConsoleKey.Backspace:
+                        start = false;
+                        break;
+                    case ConsoleKey.LeftArrow:
+                        Console.WriteLine("++LEFT++");
+                        Angle(pwm, currentAngle++);
+                        break;
+
+                    case ConsoleKey.RightArrow:
+                        Console.WriteLine("--RIGHT--");
+                        Angle(pwm, currentAngle--);
+                        break;
+                    case ConsoleKey.UpArrow:
+                        Console.WriteLine("++UP++");
+                        Angle(pwm, currentAngle++);
+                        break;
+                    case ConsoleKey.DownArrow:
+                        Console.WriteLine("--DOWN--");
+                        Angle(pwm, currentAngle--);
+                        break;
+                    default:
+                        start = false;
+                        break;
+                }
+            }
+            
         }
 
         internal static void Angle(PwmChannel pwm, int angle)
         {
-            //360° = 1050ms
+            //360° ~= 1050ms workaround for broken servo
+            //mindest winkel 1050 * 1°/360° ~= 20° und nicht 1° leider
             pwm.Start();
-            Thread.Sleep();
+            Thread.Sleep(100);
             pwm.Stop();
         }
     }
