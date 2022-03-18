234908e506425d3104531106e4b4f0432f808236
Amir Awak <amir.kian@awak.ch>
2022-03-18 23:49:03 +0100

    i am professional :P

--- Program.cs
+++ Program.cs
@@ -1,16 +1,24 @@
-﻿using Iot.Device.Uln2003;
+﻿using Innovative.SolarCalculator;
+using Iot.Device.Uln2003;
+using System.Globalization;
 
 namespace Solartracker
 {
     internal class Program
     {
+        static double DegreesToRadians(double degrees)
+        {
+            return degrees * Math.PI / 180.0;
+        }
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("--- Solartracker ---");
 
             Solartracker tracker = new Solartracker();
-            //tracker.RunGPS();
-            tracker.RunManual();
+            tracker.RunGPS();
+            //tracker.RunManual();
         }
     }
 }
--- Solartracker.cs
+++ Solartracker.cs
@@ -1,4 +1,5 @@
-﻿using Iot.Device.Uln2003;
+﻿using Innovative.SolarCalculator;
+using Iot.Device.Uln2003;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,30 +27,36 @@ namespace Solartracker
         public int AzimutAngle { get; private set; }
 
         public int Revolution { get; private set; }
+        public int MoveAngleSteps { get; private set; }
+
         public Solartracker() 
         {
             ElevationAngle = 0;
             AzimutAngle = 0;
-            Revolution = 2048;
+            Revolution = 4096;
+            MoveAngleSteps = 10;
         }
 
         public void RunManual()
         {
-            bool start = true;
-
             using (Uln2003 azimutMotor = new Uln2003(YELLOW_AZIMUT_PIN, WHITE_AZIMUT_PIN, GREEN_AZIMUT_PIN, BLUE_AZIMUT_PIN))
             using (Uln2003 elevationMotor = new Uln2003(YELLOW_ELEVATION_PIN, WHITE_ELEVATION_PIN, GREEN_ELEVATION_PIN, BLUE_ELEVATION_PIN))
             {
+                azimutMotor.Mode = StepperMode.HalfStep;
+                elevationMotor.Mode = StepperMode.HalfStep;
                 azimutMotor.RPM = 1;
-                azimutMotor.Mode = StepperMode.FullStepDualPhase;
                 elevationMotor.RPM = 1;
-                elevationMotor.Mode = StepperMode.FullStepDualPhase;
+
+                bool start = true;
+
+                Console.WriteLine($"Move Steps in Angle: {MoveAngleSteps}°");
 
                 while (start)
                 {
+                    Console.WriteLine($"Cururent Azimut Angle: {AzimutAngle}°");
+                    Console.WriteLine($"Current Elevation Angle: {ElevationAngle}°");
+
                     ConsoleKeyInfo keyinfo = Console.ReadKey(true);
-                    Console.WriteLine($"Azimut: {AzimutAngle}");
-                    Console.WriteLine($"Elevation: {ElevationAngle}");
 
                     switch (keyinfo.Key)
                     {
@@ -61,25 +68,26 @@ namespace Solartracker
                             break;
                         case ConsoleKey.LeftArrow:
                             Console.WriteLine("++LEFT++");
-                            ElevationAngle = MoveAngle(elevationMotor, ElevationAngle, 45);
+                            ElevationAngle = MoveAngle(elevationMotor, ElevationAngle, MoveAngleSteps);
                             break;
 
                         case ConsoleKey.RightArrow:
                             Console.WriteLine("--RIGHT--");
-                            ElevationAngle = MoveAngle(elevationMotor, ElevationAngle, 45, isClockwise: false);
+                            ElevationAngle = MoveAngle(elevationMotor, ElevationAngle, MoveAngleSteps, isClockwise: false);
                             break;
                         case ConsoleKey.UpArrow:
                             Console.WriteLine("++UP++");
-                            AzimutAngle = MoveAngle(azimutMotor, AzimutAngle, 45);
+                            AzimutAngle = MoveAngle(azimutMotor, AzimutAngle, MoveAngleSteps);
                             break;
                         case ConsoleKey.DownArrow:
                             Console.WriteLine("--DOWN--");
                             
-                            AzimutAngle = MoveAngle(azimutMotor, AzimutAngle, 45, isClockwise: false);
+                            AzimutAngle = MoveAngle(azimutMotor, AzimutAngle, MoveAngleSteps, isClockwise: false);
                             break;
                         default:
                             Console.WriteLine("Use Arrow keys, hit Backspace to stop");
                             start = false;
+                            azimutMotor.Stop();
                             elevationMotor.Stop();
                             break;
                     }
@@ -89,50 +97,34 @@ namespace Solartracker
 
         public void RunGPS()
         {
-            //Breitengrad Rho dezimal
-            //Längengrad lambda dezimal
-            //Deklinationswinkel Delta = a - b - c
-            //a = 0.3948 -23.2559 * cos(J' + 9.1)
-            //b = 0.3915 * cos(2*J' + 5.4)
-            //c = 0.1764 * cos(3*J'+26)
-            //J' = 360 * Tage des Jahres/Anzahl Tage im Jahr = Jahreswinkel
-            //StundenWinkel Omega 
-            //w = (12h - WOZ)* 15/h
-            //wahre ortszeit WOZ = LZ -Zeitzone + [4* lambda]min + Zgl
-            //Zgl Zeitgleichung = a + b + c nicht verwechseln mit deklinationswinkel
-            //a = [0.0066+7.3525*cos(J'+85.9)]
-            //b = [9.9359 * cos(2*J'+108.9)]
-            //c = [0.3387*cos(3*J'+105.2]
-            //Elevation
-            //lambda_Sonne = arcsin(cos w * cos rho * cos delta + sin rho * sin delta)
-            //Azimut
-            // vormittag
-            //alpha_sonne = 180 - arccos((sin y_s * sin rho - sin rho )/cos y_s * cos rho)
-            //nachmittag
-            //alpha_sonne = 180 + arccos((sin y_s * sin rho - sin rho )/cos y_s * cos rho)
             using (Uln2003 azimutMotor = new Uln2003(YELLOW_AZIMUT_PIN, WHITE_AZIMUT_PIN, GREEN_AZIMUT_PIN, BLUE_AZIMUT_PIN))
             using (Uln2003 elevationMotor = new Uln2003(YELLOW_ELEVATION_PIN, WHITE_ELEVATION_PIN, GREEN_ELEVATION_PIN, BLUE_ELEVATION_PIN))
             {
                 elevationMotor.RPM = 1;
-                elevationMotor.Mode = StepperMode.FullStepDualPhase;
+                elevationMotor.Mode = StepperMode.HalfStep;
 
+                azimutMotor.RPM = 1;
+                elevationMotor.Mode = StepperMode.HalfStep;
 
-                ElevationAngle = MoveAngle(elevationMotor, ElevationAngle, 90, isClockwise: false);
-                Thread.Sleep(2000);
-                ElevationAngle = MoveAngle(elevationMotor, ElevationAngle, 0, isClockwise: false);
-                
+                //Breitengrad Rho dezimal Züri
+                double latitude = 47.376887;
+                //Längengrad lambda dezimal Züri
+                double longitude = 8.541694;
 
+                SolarTimes solarTimes = new SolarTimes(DateTime.Now, latitude, longitude);
+                ElevationAngle = SetAngle(elevationMotor,ElevationAngle, solarTimes.SolarElevation.Degrees);
+                AzimutAngle = SetAngle(azimutMotor, AzimutAngle, solarTimes.SolarAzimuth.Degrees);
                 //PARAMS RT, DATUM, ZEIT, Längengrad, Breitengrad                
             }
         }
 
         private int MoveAngle(Uln2003 motor, int currentAngle, int angle, bool isClockwise = true)
         {
-            if (IsOutOfRadius(angle))
+            if ((isClockwise && IsOutOfRadius(currentAngle + angle)) || (!isClockwise && IsOutOfRadius(currentAngle - angle)))
             {
                 return currentAngle;
             }
-            else if (currentAngle == 0)
+            if (currentAngle == 0)
             {
                 int turnStep = (Revolution * angle) / 360;
                 motor.Step(turnStep);
@@ -148,16 +140,12 @@ namespace Solartracker
             }
             else if (isClockwise)
             {
-                Console.WriteLine("is clockwise");
-                Console.WriteLine($"Angle: {angle} CurrentAngle: {currentAngle}");
                 int turnSteps = (Revolution * angle) / 360;
                 motor.Step(turnSteps);
                 return angle + currentAngle;
             }
             else
             {
-                Console.WriteLine("is counter clockwise");
-                Console.WriteLine($"Angle: {angle} CurrentAngle: {currentAngle}");
                 int turnSteps = (Revolution * angle) / 360;
                 motor.Step(-turnSteps);
                 return currentAngle - angle;
@@ -173,25 +161,25 @@ namespace Solartracker
             }
             else if (currentAngle == 0)
             {
-                int turnStep = (2048 * angle) / 360;
+                int turnStep = (Revolution * angle) / 360;
                 motor.Step(turnStep);
             }
             else if (angle == 0)
             {
-                int turnStep = (2048 * currentAngle) / 360;
+                int turnStep = (Revolution * currentAngle) / 360;
                 motor.Step(-turnStep);
             }
             else if (angle < currentAngle)
             {
-                int currentSteps = (2048 * currentAngle) / 360;
-                int newAngleSteps = (2048 * angle) / 360;
+                int currentSteps = (Revolution * currentAngle) / 360;
+                int newAngleSteps = (Revolution * angle) / 360;
                 int turnStepsBack = currentSteps - newAngleSteps;
                 motor.Step(-turnStepsBack);
             }
             else if (angle > currentAngle)
             {
-                int currentSteps = (2048 * currentAngle) / 360;
-                int newAngleSteps = (2048 * angle) / 360;
+                int currentSteps = (Revolution * currentAngle) / 360;
+                int newAngleSteps = (Revolution * angle) / 360;
                 int turnStepsForward = newAngleSteps - currentSteps;
 
                 motor.Step(turnStepsForward);
@@ -202,7 +190,12 @@ namespace Solartracker
 
         private bool IsOutOfRadius(int angle)
         {
-            return (angle < 0 || angle> 360);
+            if (angle < 0 || angle > 360)
+            {
+                Console.WriteLine($"angle: {angle}° is out of range 0 < x < 360");
+                return true;
+            }
+            return false;
         }
     }
 }
